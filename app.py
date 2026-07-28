de253941b296f15f35eb076116ab164f26d4724c
micoleolivia <micoledmochowska@gmail.com>
2026-07-28 21:46:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,6 @@ using the **WHO/UNICEF Estimates of National Immunization Coverage (WUENIC)**.
 """)
 
 st.info("""
-**About this Dashboard**
-
 This dashboard analyses **DTP3 vaccination coverage**, a key indicator used by the
 World Health Organization (WHO) and UNICEF to monitor the performance of routine
 childhood immunisation programmes worldwide.
